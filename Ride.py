d5204c2b4350046a3cd677dc73e8efaf719227f6
Sias <15emiliomurray@gmail.com>
2024-12-13 15:09:55 +0200

    first and final

--- Ride.py
+++ Ride.py
@@ -9,12 +9,15 @@ st.text_input("How many people can the ride take per cycle?",key="capacity")
 
 
 def queue_time():
-    people_in_queue = int(st.session_state["people"])
-    ride_duration = int(st.session_state["duration"])
-    capacity_per_cycle = int(st.session_state["capacity"])
-    cycles_needed = math.ceil(people_in_queue/capacity_per_cycle)
-    total_time = cycles_needed * ride_duration
-    st.text(f"Estimated total wait time for the queue: {total_time}m")
+    try:
+        people_in_queue = int(st.session_state["people"])
+        ride_duration = int(st.session_state["duration"])
+        capacity_per_cycle = int(st.session_state["capacity"])
+        cycles_needed = math.ceil(people_in_queue/capacity_per_cycle)
+        total_time = cycles_needed * ride_duration
+        st.text(f"Estimated total wait time for the queue: {total_time}m")
+    except:
+        st.text("Please only put full numbers in the text boxes.")
 
 
 
